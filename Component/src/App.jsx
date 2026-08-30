ffa0fbe3dd4a085d352ba103a9c6dc77b89fb12c
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-30 20:24:11 +0530

    components

---
+++ Component/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './components/card'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Card />
+    </div>
+  )
+}
+
+export default App
