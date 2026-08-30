158f3a7eeca5064ddd50e085af036f1bd761ab46
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-08-30 21:02:50 +0530

    props done

--- prpos/src/App.jsx
+++ prpos/src/App.jsx
@@ -4,7 +4,8 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className='parent'>
-      <Card user = 'navdeep' age={18} />
+      <Card user = 'navdeep' img="https://images.unsplash.com/photo-1779896412071-bf3160aad19e?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8" />
+      <Card user = 'nilkashi' img="https://images.unsplash.com/photo-1784964279455-f0590dfb6694?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
     </div>
   )
 }
