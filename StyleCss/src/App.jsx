07a8dbda5447da44010242328b5e01670746f23c
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-04 20:53:15 +0530

    css types learning

---
+++ StyleCss/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button/Button/Button"
+import Header from "./components/Button/Header/Header"
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Button />
+    </>
+  )
+}
+
+export default App
---
+++ StyleCss/src/components/Button/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>This is header</div>
+  )
+}
+
+export default Header
