36f93ab18338e3a733bfafb2e32f51cab419b2a7
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-05 21:42:28 +0530

    function learning

---
+++ Functionss/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  // const click = () => {
+  //   alert('button is clicked')
+  // }
+
+  // function oninput(){
+  //   console.log('Input changing');
+  // }
+
+  return (
+    <div>
+      {/* <button onClick={click}>click</button> */}
+      {/* <input onChange={oninput} type="text" />Type */}
+    </div>
+  )
+}
+
+export default App
