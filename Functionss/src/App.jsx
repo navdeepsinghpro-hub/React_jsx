dd6862fcb2029da601af7b0014fa1389d3808d2c
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-06 21:15:45 +0530

    functions

--- Functionss/src/App.jsx
+++ Functionss/src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
     <div>
       {/* <button onClick={click}>click</button> */}
       {/* <input onChange={oninput} type="text" />Type */}
+      <div></div>
     </div>
   )
 }
