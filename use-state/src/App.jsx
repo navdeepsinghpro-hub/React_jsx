22233ecf25bb456e7f426fc5a6ac5f309571e80b
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-08 19:52:42 +0530

    event handling

--- use-state/src/App.jsx
+++ use-state/src/App.jsx
@@ -2,21 +2,34 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-const [num, setnum] = useState(0)
+// const [num, setnum] = useState(0)
 
-function inc(){
-  setnum(num+1);
-}
+// function inc(){
+//   setnum(num+1);
+// }
 
-function dec(){
-  setnum(num-1);
+// function dec(){
+//   setnum(num-1);
+// }
+
+const [num, setnum] = useState({user:'navdeep', age:18})
+
+const btnclick = () => {
+  const newNum = {...num}
+
+  newNum.age = 19
+  newNum.user = 'gopi'
+
+  setnum(newNum)
 }
 
   return (
     <div>
-      <h1>{num}</h1>
+      {/* <h1>{num}</h1>
       <button onClick={inc}>Increase</button>
-      <button onClick={dec}>Decrease</button>
+      <button onClick={dec}>Decrease</button> */}
+      <h1> {num.user}, {num.age} </h1>
+      <button onClick={btnclick}>click</button>
     </div>
   )
 }
