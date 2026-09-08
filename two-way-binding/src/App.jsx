0274ce7ee7d04a40672aea0a0f3bb53876bec873
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-08 20:29:22 +0530

    two_way_binding

---
+++ two-way-binding/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Suspense, useState } from 'react'
+
+const App = () => {
+
+  const [title, settitle] = useState('')
+
+  const submitHandle = (e) => {
+    e.preventDefault()
+    console.log("done")
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+          submitHandle(e)
+        }}>
+        <input type="text" placeholder='Enter name'
+        value={title} onChange={(elem)=>{
+          settitle(elem.target.value)
+        }}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
