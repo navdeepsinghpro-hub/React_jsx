b63622499512bb30982c56600b8626c1510b3b82
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-21 21:13:28 +0530

    project

--- Note_project/src/App.jsx
+++ Note_project/src/App.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react'
+import { X } from 'lucide-react';
 
 const App = () => {
   const [title, setTitle] = useState('')
-  const [details, setDetails] = useState()
+  const [details, setDetails] = useState('')
+  const [task, setTask] = useState([])
 
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log(title);
+
+    const copyTask = [...task];
+
+    copyTask.push({title,details})
+    setTask(copyTask)
+
     setTitle('')
     setDetails('')
+
+    const deleteNote = () => {
+      
+    }
   }
 
 
@@ -40,15 +51,24 @@ const App = () => {
             }}
            />
 
-          <button className='bg-white font-medium w-full text-black px-5 py-2 rounded'>Add Note</button>
+          <button 
+            className='bg-white active:scale-95 font-medium w-full text-black px-5 py-2 rounded'
+            >
+             Add Note
+          </button>
 
       </form>
 
-      <div className='lg:w-1/2 lg:border-l-2 p-10 h-full overflow-auto'>
+      <div className='lg:w-1/2 lg:border-l-2 p-10'>
         <h1 className='text-4xl font-bold'>Recent Notes</h1>
-        <div className='flex flex-wrap gap-5 mt-5 '>
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-6 h-full overflow-auto'>
+          {task.map(function(elem,idx){
+            return <div key={idx} className=" relative h-52 w-40 rounded-2xl text-black cover bg-[url('https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTA4L3Jhd3BpeGVsX29mZmljZV8yNV9hX2N1dGVfM2RfYmxhbmtfc3RpY2t5X3Bvc3RfaXRfbm90ZV93aXRoX3RodV8wYTg4Y2U2Zi0zYjBiLTQ5NmItODUxOC0zZGExOTMyNzhkYWQucG5n.png)] p-4 bg-white">
+              <h2 onClick={deleteNote} className='absolute top-5 right-5 bg-red-500 p-1 rounded-full text-xs'><X size={16} strokeWidth={2.75} /></h2>
+              <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
+              <p className='mt-2 leading-tight font-medium text-gray-500'>{elem.details}</p>
+            </div>
+          })}
         </div>
       </div>
     </div>
