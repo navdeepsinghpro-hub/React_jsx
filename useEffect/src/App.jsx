ea101d2d1fa58b9d145c900cf08f68757bf8d103
navdeepsinghpro-hub <navdeepsinghpro@gmail.com>
2026-09-26 14:30:41 +0530

    done

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
 
+  const [a, setA] = useState(0)
+  const [b, setB] = useState(0)
+
+  function A(){
+    setA(b+1)
+  }
+
+  function B(){
+    setB(b+1)
+  }
+
   useEffect(function(){
-    console.log("Use running")
-  })
+    console.log("a is changed")
+  }, [a])
 
   return (
-    <div></div>
+    <div>
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <button onClick={A}>A</button>
+      <button onClick={B}>B</button>
+    </div>
   )
 }
 
